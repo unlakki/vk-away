c7c238f9ef582e508826240e9886d620299d61c8
Uwufule <39487012+uwufule@users.noreply.github.com>
2020-01-03 23:53:45 +0300

    Update @namespace

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VK Away
-// @namespace    https://github.com/ferisef/
+// @namespace    https://github.com/uwufule/
 // @version      1.1.1
 // @description  Обход /away.php вконтача.
 // @author       FuyukaiDesu
